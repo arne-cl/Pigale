26ae4c0e75a2ee538c5651d150d60eccb864e9e3
hbonnin <hbonnin>
2002-04-29 08:05:00 +0000

    *** empty log message ***

--- qt/Settings.cpp
+++ qt/Settings.cpp
@@ -1,3 +1,14 @@
+/****************************************************************************
+**
+** Copyright (C) 2001 Hubert de Fraysseix, Patrice Ossona de Mendez.
+** All rights reserved.
+** This file is part of the PIGALE Toolkit.
+**
+** This file may be distributed under the terms of the GNU Public License
+** appearing in the file LICENSE.HTML included in the packaging of this file.
+**
+*****************************************************************************/
+
 #include <qapplication.h>
 #include "MyWindow.h"
 #include "mouse_actions.h"
