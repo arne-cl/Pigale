8c1a44fd049ad53b061c73972933244a28f0c63b
hbonnin <hbonnin>
2002-05-06 11:38:52 +0000

    test3 shows the properties of the current graph

--- qt/Test.cpp
+++ qt/Test.cpp
@@ -1,5 +1,6 @@
 #include <qapplication.h>
 #include <TAXI/graphs.h> 
+#include <TAXI/propdef.h>
 
 
 void Test(GraphContainer &GC,int action)
@@ -17,7 +18,36 @@ void Test(GraphContainer &GC,int action)
 	  qDebug("edge:%3.3d  %3.3d %3.3d",e(),G.vin[e](),G.vin[-e]());
       return;
       }
-  if(action == 3) //save settings
-      ;
+  if(action == 3) 
+      // display  the properties of the current graph that would be saved in a tgf file.
+    {qDebug("Vertices:");
+    for (int i=G.Set(tvertex()).PStart(); i<G.Set(tvertex()).PEnd(); i++)
+      if (G.Set(tvertex()).exist(i))
+	      if (G.Set(tvertex()).defined(i)) 
+		  qDebug("  %d %s (%s) -> %d bytes",i,PropName(1,i),PropDesc(1,i),
+			 (1+G.nv())*G.Set(tvertex())(i)->size_elmt());
+	      else
+		  qDebug("  %d %s (%s)",i,PropName(1,i),PropDesc(1,i));
+    qDebug("Edges:");
+    for (int i=G.Set(tedge()).PStart(); i<G.Set(tedge()).PEnd(); i++)
+      if (G.Set(tedge()).exist(i))
+	      if (G.Set(tedge()).defined(i)) 
+		  qDebug("  %d %s (%s) -> %d bytes",i,PropName(2,i),PropDesc(2,i),
+			 (1+G.ne())*G.Set(tedge())(i)->size_elmt());
+	      else
+		  qDebug("  %d %s (%s)",i,PropName(2,i),PropDesc(2,i));
+    qDebug("Brins (half-edges):");
+    for (int i=G.Set(tbrin()).PStart(); i<G.Set(tbrin()).PEnd(); i++)
+      if (G.Set(tbrin()).exist(i))
+	      if (G.Set(tbrin()).defined(i)) 
+		  qDebug("  %d %s (%s) -> %d bytes",i,PropName(3,i),PropDesc(3,i),
+			 (1+2*G.ne())*G.Set(tbrin())(i)->size_elmt());
+	      else
+		  qDebug("  %d %s (%s)",i,PropName(3,i),PropDesc(3,i));
+    qDebug("General:");
+    for (int i=G.Set().PStart(); i<G.Set().PEnd(); i++)
+      if (G.Set().exist(i))
+	  qDebug("  %d %s (%s)",i,PropName(0,i),PropDesc(0,i));
+    }
   }
 
