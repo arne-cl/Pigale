40e0e66fb85dae97edfe6486adf5dc8d1e86bcc5
hbonnin <hbonnin>
2002-06-28 12:47:58 +0000

    added tfunctions to code rgb values into an unsigned int

--- incl/TAXI/color.h
+++ incl/TAXI/color.h
@@ -30,4 +30,14 @@ enum {
   Ivory  = 16 
 };
 
+// Functions to code and decode an rgb value into an unsigned int
+inline int GetRed(unsigned int rgb)              // get red part of RGB
+{return (int)((rgb >> 16) & 0xff);}
+inline int GetGreen(unsigned int rgb)            // get green part of RGB
+{return (int)((rgb >> 8) & 0xff);}
+inline int GetBlue(unsigned int rgb)             // get blue part of RGB
+{return (int)(rgb & 0xff);}
+inline unsigned int SetRgb(int r,int g,int b )   // set RGB value
+{return (0xff << 24) | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);}
+
 #endif
