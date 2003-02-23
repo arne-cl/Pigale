a661d370d4a177f4837fa045f804278df9d4ec95
hbonnin <hbonnin>
2003-02-23 11:35:49 +0000

    changing the organisation of files

--- ClientServer/xserver.h
+++ ClientServer/xserver.h
@@ -9,56 +9,12 @@
 **
 *****************************************************************************/
 
-#ifndef SERVER_H 
-#define SERVER_H
+#ifndef XSERVER_H 
+#define XSERVER_H
 
-#include <qsocket.h>
-#include <qserversocket.h>
 #include <qapplication.h>
 #include <qvbox.h>
-#include <qtextview.h>
-#include <qlabel.h>
-#include <qpushbutton.h>
-#include <qradiobutton.h>
-#include <qtextstream.h>
-#include <qstringlist.h>
-#include <stdlib.h>
-
-#include <Pigale.h>
-#include "MyWindow.h"
-#include <QT/Misc.h>
-#include <QT/Action_def.h>
-
-
-class ClientSocket : public QSocket
-// created each time a client connects
-{Q_OBJECT
-public:
- ClientSocket(int sock,int nid,bool show,QObject *parent=0,const char *name=0);
-  ~ClientSocket() { }
-  
-
-signals:
-  void logText(const QString&);
-  void signalId(int);
-
-private slots:
-  void readClient();
-  void ClientClosed();
-
-private: 
-  void sendServerId();
-  void writeServer(QString&  msg);
-  int parseSplitAction(const QString& data);
-  int xhandler(const QString& data);
-  int handlerInfo(int action);
-  int handlerInput(int action,const QString& data);
-  int line;
-  int prId;
-  int sdebug;
-  QTextStream cli;
-  MyWindow *mw; 
-};
+#include "ClientSocket.h"
 
 class SimpleServer : public QServerSocket
 // create a ClientSocket each time a client connects
