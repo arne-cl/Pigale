78d987c6069e3cd2dd278b8ea9d82d19d45575c8
hbonnin <hbonnin>
2003-02-26 20:45:52 +0000

    *** empty log message ***

--- qt/MyWindow.cpp
+++ qt/MyWindow.cpp
@@ -940,6 +940,7 @@ int MyWindow::handler(int action)
   // 3:(Drawing) 4:(3d) 5:symetrie 6-7-8:Springs Embedders
   if(ret < 0)return 0;
   double Time = t.elapsed()/1000.;
+  qDebug("ret=%d",ret);
   if(ret == 1)
       {if(!MacroExecuting )
 	  gw->update();
@@ -954,8 +955,6 @@ int MyWindow::handler(int action)
       if(!MacroExecuting )
 	  gw->update(0);
       }
-  else if(ret == 21)
-      if(!MacroRecording)information();
   else if(ret == 3)
       mypaint->update(drawing); 
   else if(ret == 4) //3d
