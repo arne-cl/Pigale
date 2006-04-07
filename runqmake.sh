168990b21fded344158377883fa4a3471559e061
hbonnin <hbonnin>
2006-04-07 15:45:23 +0000

    pigale 1.2.3 using Qt4 new installation files

---
+++ runqmake.sh
@@ -0,0 +1,11 @@
+echo "Running qmake..."
+
+if test -f qt4dir ; then
+  QT4DIR=`cat qt4dir`
+  export QT4DIR
+fi
+
+QMAKE=$QT4DIR/bin/qmake
+${QMAKE} -o tgraph/Makefile  tgraph/tgraph.pro
+${QMAKE} -o qt/Makefile  qt/pigale.pro
+${QMAKE} -o ClientServer/Makefile  ClientServer/client.pro
