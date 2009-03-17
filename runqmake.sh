5d9bf3e57cc94677bfb2b63eb67feffa53df9788
hbonnin <hbonnin>
2009-03-17 16:47:31 +0000

    added to configure the option --enable-ubuntu which allows to compile pigale using the Qt4 package installed on Ubuntu distributions.

--- runqmake.sh
+++ runqmake.sh
@@ -4,8 +4,15 @@ if test -f qt4dir ; then
   QT4DIR=`cat qt4dir`
   export QT4DIR
 fi
+if test -f system ; then
+  SYSTEM=`cat system`
+fi
+if [ $SYSTEM  = "ubuntu" ]; then
+    QMAKE=/usr/bin/qmake-qt4
+else
+    QMAKE=$QT4DIR/bin/qmake
+fi
 
-QMAKE=$QT4DIR/bin/qmake
 ${QMAKE} -o tgraph/Makefile  tgraph/tgraph.pro
 ${QMAKE} -o freeglut/Makefile  freeglut/freeglut.pro
 ${QMAKE} -o qt/Makefile  qt/pigale.pro
